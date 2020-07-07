7f0532eda00a60d4e66ae65e712ff989495d524a
doxas <eizanco@gmail.com>
2020-07-08 01:32:10 +0900

    add appbar component in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/app.scss';
+import ButtonAppBar from '../components/ButtonAppBar.js';
 
 export default function App({Component, pageProps}){
-    return <Component {...pageProps} />;
+    return (
+        <div>
+            <ButtonAppBar />
+            <Component {...pageProps} />
+        </div>
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
+import Link from 'next/link'
+
 import Button from '@material-ui/core/Button';
 
 function HomePage(){
     return (
         <div>
             <div>Welcome to <span>Next.js</span>!</div>
+            <Link href="./about">
+                <a>ABOUT</a>
+            </Link>
             <Button variant="contained" color="primary">Hello World</Button>
-
             <style jsx>{`span {color: red;}`}</style>
         </div>
     );
