ae06b27df842ebf446bc961d5fd1af4587f5dcd2
doxas <eizanco@gmail.com>
2020-07-11 21:25:45 +0900

    improve dynamic import

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,27 @@
+import dynamic from 'next/dynamic'
 import Head from 'next/head';
 import Link from 'next/link';
 
 import UserMenu from '../components/UserMenu/UserMenu.js';
 import css from '../styles/index.scss';
 
-export default function Index(){
+export default function Index(props){
+    const DinamicRenderingCanvasNoSSR = dynamic(() => {
+        return import('../components/RenderingCanvas/RenderingCanvas.js')
+        .then(() => {
+            console.log('🐠: canvas loaded', props);
+        });
+    }, {
+        ssr: false
+    });
+
     return (
-        <div className={css.indexwrap}>
+        <div className={css.wrap}>
             <Head>
                 <title>top</title>
                 <script src="./vendor/script.js"></script>
             </Head>
-            <div className={css.webglwrap}>
-                <canvas className={css.webglcanvas}></canvas>
-            </div>
+            <DinamicRenderingCanvasNoSSR />
             <div className={css.interfacewrap}>
                 interfaces.
                 <UserMenu
